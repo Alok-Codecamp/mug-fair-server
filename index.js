3614580ee2b01e38d4ebb73ea5eec0a0246157f9
Alok Kumar <alok61.bd@gmail.com>
2021-11-14 22:52:55 +0600

    add a comment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrwnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// run function 
 async function run(){
     try{
         await client.connect();
